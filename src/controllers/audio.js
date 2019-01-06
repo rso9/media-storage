896e1d7c834436bcdb461d8b966317cc530c7a33
Jaka Stavanja <jakastavanja@gmail.com>
2019-01-06 14:26:01 +0100

    Add discovery debug path for testing

--- src/controllers/audio.js
+++ src/controllers/audio.js
@@ -2,6 +2,7 @@ import path from 'path'
 import mediaserver from 'mediaserver'
 import HttpStatus from 'http-status-codes'
 import axios from 'axios'
+const KumuluzeeDiscovery = require('@kumuluz/kumuluzee-discovery').default
 
 const w = require('winston')
 
@@ -20,33 +21,56 @@ audioController.serveSong = (req, res) => {
 
 }
 
-audioController.uploadSong = (req, res) => {
+audioController.uploadSong = async (req, res) => {
 
     const artistId = parseInt(req.body.artist)
     const songTitle = req.body.title
 
     w.log('info', 'UPLOAD SONG ENDPOINT', {songUploaded: req.file.originalname, markerName: 'ENTRY'})
+
     if (req.file.error) {
+
         w.log('error', 'UPLOAD SONG ENDPOINT', {songUploaded: req.file.originalname, markerName: 'EXIT', error: req.file.error})
         res.status(req.file.error.status).send(req.file.error)
-    } else {
-        const song = req.file.originalname;
 
-        axios.post('http://localhost:8080/v1/song', {
-            songName: songTitle,
-            artists: [{id: artistId}],
-            songUrl: '/song/' + song
-        })
-            .then(res => res.json())
-            .then(response => console.log(response))
-            .catch(err => {
-                w.log('error', 'UPLOAD SONG ENDPOINT', {songUploaded: req.file.originalname, markerName: 'EXIT', error: err.response.data})
-                res.status(500).send(err)
-                res.end()
+    } else {
+        
+        const song = req.file.originalname;
+        let internalURL = "configissue"
+
+        try {
+            internalURL = await KumuluzeeDiscovery.discoverService({
+                value: "catalog",
+                version: "1.0.0",
+                environment: "dev",
+                accessType: "DIRECT"
             })
 
-        res.status(HttpStatus.OK)
+            console.log("internal catalog url: " + internalURL)
+
+            axios.post(internalURL + 'v1/song', {
+                songName: songTitle,
+                artists: [{id: artistId}],
+                songUrl: '/song/' + song
+            })
+                .then(res => res.json())
+                .then(response => console.log(response))
+                .catch(err => {
+                    w.log('error', 'UPLOAD SONG ENDPOINT', {songUploaded: req.file.originalname, markerName: 'EXIT', error: err.response.data})
+                    res.status(500).send(err)
+                    res.end()
+                })
+    
+            res.status(HttpStatus.OK)
+
+        } catch (err) {
+            w.log('error', 'UPLOAD SONG ENDPOINT', {songUploaded: req.file.originalname, markerName: 'EXIT', error: err})
+            res.status(500).send(err)
+            res.end()
+        }
+
     }
+    
     w.log('info', 'UPLOAD SONG ENDPOINT', {songUploaded: req.file.originalname, markerName: 'EXIT'})
     res.end()
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,23 @@
 import express from 'express'
 import audioController from '../controllers/audio'
 import upload from '../utils/multer/upload'
+const KumuluzeeDiscovery = require('@kumuluz/kumuluzee-discovery').default
 
 const router = express.Router()
 
 // song routes
 router.get('/song/:name', audioController.serveSong)
 router.post('/song', upload.single('song'), audioController.uploadSong)
+router.get('/test', async (req, res) => {
+  internalURL = await KumuluzeeDiscovery.discoverService({
+    value: "catalog",
+    version: "1.0.0",
+    environment: "dev",
+    accessType: "DIRECT"
+  })
+
+  console.log(internalURL)
+  res.json({oj})
+})
 
 export default router
